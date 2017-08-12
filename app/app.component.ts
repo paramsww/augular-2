8f51bcb9b4c8501227871fb9999d1c72d60afd21
paramsww <paramsww@gmail.com>
2017-08-12 11:11:09 +0530

    hi

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'app-app',
+/*template: `<div> <h1>This Is My Page</h1>
+<book-app></book-app>
+</div>`
+templateUrl: 'app/book.component.html'*/
+
+template : '<book-app></book-app>'
+
+})
+
+export class AppComponent{}
---
+++ app/book.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+selector:'book-app',
+templateUrl: 'app/book.component.html'
+
+})
+export class BookComponent{}
+
